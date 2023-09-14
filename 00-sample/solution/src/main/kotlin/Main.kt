d6929a104bc693bbe418a161b1c95f4e15e47ff5
Elisey Vasilevskiy <elisey.vasilevskiy@corp.mail.ru>
2023-09-14 22:59:12 +0300

    add task 1

--- 00-sample/solution/src/main/kotlin/Main.kt
+++
@@ -1,8 +0,0 @@
-fun main() {
-    println("Тут лежит решение")
-    Thread.sleep(1000)
-    println("""
-        Обратите внимание, решение лежит в папке solution, а не в папке solution/MyProject/
-        Файлы за пределами папки solution трогать НЕ надо.
-    """.trimIndent())
-}
