14afc784090330a2d7ffef9eb197dff644ffe889
daniil1112 <moiplansh028@gmail.com>
2023-09-20 11:02:47 +0300

    Add state for message

---
+++ src/main/java/company/vk/polis/task1/State.kt
@@ -0,0 +1,19 @@
+package company.vk.polis.task1
+
+class State(private val state: StateEnum) {
+    private var deletedBy: Long? = null
+
+    constructor(state: StateEnum, deletedBy: Long?) : this(state) {
+        if (deletedBy != null && state != StateEnum.DELETED) {
+            throw IllegalArgumentException("State can't be with deleted id");
+        }
+        this.deletedBy = deletedBy;
+    }
+
+    fun getDeletedBy(): Long? = deletedBy
+    fun getState(): StateEnum = state
+}
+
+enum class StateEnum {
+    READ, UNREAD, DELETED
+}
