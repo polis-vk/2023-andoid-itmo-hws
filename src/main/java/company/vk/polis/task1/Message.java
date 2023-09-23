3ae8ee347f5662e75f49e0f5555711a70ae6f606
Andrey Verenya <verenya08@gmail.com>
2023-09-23 23:18:33 +0300

    Remove Deprecated

--- src/main/java/company/vk/polis/task1/Message.java
+++ src/main/java/company/vk/polis/task1/Message.java
@@ -1,12 +1,6 @@
 package company.vk.polis.task1;
 
 public record Message(Integer id, String text, Integer senderId, Long timestamp, MessageState state) implements Entity {
-    @Deprecated
-    Message(Integer id, String text, Integer senderId, Long timestamp)
-    {
-        this(id, text, senderId, timestamp, null);
-    }
-
     @Override
     public Integer getId() {
         return id;
