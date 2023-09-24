af0ae765389876172be072242b33e2dcf98b5740
Andrey Verenya <verenya08@gmail.com>
2023-09-24 13:53:05 +0300

    Main

--- src/main/java/company/vk/polis/task1/Main.kt
+++ src/main/java/company/vk/polis/task1/Main.kt
@@ -1,5 +1,19 @@
 package company.vk.polis.task1
 
 fun main() {
-    //TODO()
+    val info = Repository.getInfo()
+    val messageController = MessageController(info)
+    val userId = 1
+
+    val preview = messageController.getPreviewChats(userId)
+
+    println("Preview chats by userId $userId:")
+    for (i in 0 until minOf(10, preview.size)) {
+        val chatItem = preview[i]
+        println("Element $i: Avatar URL: ${chatItem.avatarUrl?.padEnd(3) ?: "N/A".padEnd(3)}, Label: ${chatItem.label}")
+    }
+    println()
+
+    val count = messageController.countMessagesByUserId(1)
+    println("Count message by userId $userId: $count")
 }
