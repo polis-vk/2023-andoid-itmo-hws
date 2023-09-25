5050663e850dae600fd26ddfe7bb089c832a3dfa
dandobri <79245684+dandobri@users.noreply.github.com>
2023-09-26 01:15:23 +0300

    Update Message_Controller.kt

--- src/main/java/company/vk/polis/task1/Message_Controller.kt
+++ src/main/java/company/vk/polis/task1/Message_Controller.kt
@@ -1,24 +1,33 @@
 package company.vk.polis.task1
 
-class Message_Controller {
-    private val entities: List<Entity> = Repository.getInfo().filter { it -> it.isValid };
-    fun get_ChatItem(user_id: Int?, state: State_Msg?) :MutableList<ChatItem> {
-        val chats: List<Chat> = entities.filterIsInstance<Chat>().filter { it -> (it.userIds.senderId == user_id || it.userIds.receiverId == user_id)};
-        val user_chat_item: MutableList<ChatItem> = mutableListOf();
-        var i: Int = 0;
-        for(chat in chats) {
-            val chat_messages_Ids: List<Int> = chat.messageIds;
-            val messages: List<Message> = entities.filterIsInstance<Message>().filter { it -> (it.id in chat_messages_Ids && it.state == state)};
-            val list_chat_msg: List<Message> = messages.sortedBy { it -> it.timestamp };
-            val last_msg: String? = list_chat_msg[list_chat_msg.size - 1].text;
-            val list_user_url: List<User> = entities.filterIsInstance<User>().filter { it -> it.id == user_id };
-            val user_url: String? = list_user_url.get(0).avatarUrl;
-            user_chat_item.add(++i, ChatItem(user_url, last_msg, state?.get_state(), user_id));
+class MessageController {
+    private val entities = Repository.getInfo().filter { isValid(it) }
+
+    private fun isValid(entity: Entity) : Boolean {
+        return when(entity) {
+            is Chat -> entity.id != null && entity.userIds != null && entity.messageIds != null
+            is Message -> entity.id != null && entity.text != null && entity.senderId != null && entity.timestamp != null && entity.state != null
+            is User -> entity.id != null && entity.name != null
+            else -> false
         }
-        return user_chat_item;
     }
-    fun count_sended_Messages(user_id: Int?) :Int {
-        val chats: List<Chat> = entities.filterIsInstance<Chat>().filter { it -> it.userIds.receiverId == user_id};
+
+    fun getChatItem(user_id: Int?, state: StateMsg?) : ArrayList<ChatItem> {
+        val chats = entities.filterIsInstance<Chat>().filter { it.userIds.senderId == user_id || it.userIds.receiverId == user_id }
+        val userChatItem = ArrayList<ChatItem>()
+        for(chat in chats) {
+            val chatMessagesIds = chat.messageIds;
+            val messages = entities.filterIsInstance<Message>().filter { it.id in chatMessagesIds && it.state == state }
+            val listChatMsg = messages.sortedBy { it -> it.timestamp }
+            val lastMsg : String? = listChatMsg[listChatMsg.size - 1].text
+            val listUserUrl = entities.filterIsInstance<User>().filter { it.id == user_id }
+            val userUrl : String? = listUserUrl[0].avatarUrl
+            userChatItem.add(ChatItem(userUrl, lastMsg, state!!, user_id))
+        }
+        return userChatItem
+    }
+    fun countSendedMessages(userId: Int?) :Int {
+        val chats = entities.filterIsInstance<Chat>().filter { it.userIds.receiverId == userId }
         return chats.size;
     }
 }
