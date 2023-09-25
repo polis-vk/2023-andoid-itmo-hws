3a5b0fa19c9f23afe6869f2b8a1a0fd24601941a
dandobri <79245684+dandobri@users.noreply.github.com>
2023-09-26 01:14:52 +0300

    Update Message.java

--- src/main/java/company/vk/polis/task1/Message.java
+++ src/main/java/company/vk/polis/task1/Message.java
@@ -1,12 +1,8 @@
 package company.vk.polis.task1;
 
-record Message(Integer id, String text, Integer senderId, Long timestamp, State_Msg state) implements Entity {
+record Message(Integer id, String text, Integer senderId, Long timestamp, StateMsg state) implements Entity {
     @Override
     public Integer getId() {
         return id;
-    }
-    @Override
-    public Boolean isValid() {
-        return id != null && text != null && senderId != null && timestamp != null && state != null;
     }
 }
