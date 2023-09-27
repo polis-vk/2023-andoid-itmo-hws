00eefee9842fcb14c219819d4d8e3f4b8af7f18f
Ultimatereo <mansursult9@gmail.com>
2023-09-27 18:46:22 +0300

    Deleted useless annotation

--- src/main/java/company/vk/polis/task1/GroupChat.kt
+++ src/main/java/company/vk/polis/task1/GroupChat.kt
@@ -6,7 +6,7 @@ data class GroupChat(
     val groupChatId: Int?,
     override val userIds: List<Int?>?,
     override val messageIds: List<Int?>?,
-    @Nullable val avatarUrl: String?,
+    val avatarUrl: String?,
 ) : ChatInterface {
     override fun getId(): Int? {
         return groupChatId
