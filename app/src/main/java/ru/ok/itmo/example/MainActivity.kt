b583be97bcb1c6353a9b49d66063b60f9ec17a26
Kozlov_Dmitrii <368321@niuitmo.ru>
2023-10-02 19:16:10 +0200

    my solution without locale

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,13 +1,9 @@
 package ru.ok.itmo.example
 
-import android.content.DialogInterface
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
 import android.text.method.HideReturnsTransformationMethod
 import android.text.method.PasswordTransformationMethod
-import android.view.Gravity
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.view.View
@@ -20,85 +16,73 @@ import androidx.appcompat.app.AppCompatDelegate
 // проверил на паре эсмуляторов и на своём телефоне Redmi Note 8
 class MainActivity : AppCompatActivity() {
 
-    var etEmail: EditText? = null
-    var etPassword: EditText? = null
-    var themeButt : Button? = null
+    private lateinit var etEmail: EditText;
+    private lateinit var etPassword: EditText
+    private lateinit var themeButt: Button
+
     //так как у editText есть id, то уже введённые данные при смене ориентации остаются. Вроде...
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_main)
-        val enter = fromScreenOrientation(findViewById<Button>(R.id.enter1), findViewById<Button>(R.id.enter2))
+        val enter = findViewById<Button>(R.id.enter)
         etEmail = findViewById(R.id.username)
         etPassword = findViewById(R.id.password)
-        val hide = findViewById<Button>(R.id.hide)
+        val visiblePassBtn = findViewById<Button>(R.id.visiblePassBtn)
         themeButt = findViewById(R.id.theme)
-        enter.setOnClickListener(object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                var allOk = CheckFields()
-                if (allOk) {
-                    Toast.makeText(applicationContext, "Успех", Toast.LENGTH_SHORT).show()
-                }
+        enter.setOnClickListener {
+            val allOk = checkFields()
+            if (allOk) {
+                Toast.makeText(applicationContext, getString(R.string.succes), Toast.LENGTH_SHORT)
+                    .show()
             }
-        })
+        }
 
-        etPassword!!.setOnKeyListener { v, keyCode, event ->
+        etPassword.setOnKeyListener { v, keyCode, event ->
             when {
                 ((keyCode == KeyEvent.KEYCODE_ENTER) && (event.action == KeyEvent.ACTION_DOWN)) -> {
                     enter.performClick()
                     return@setOnKeyListener true
                 }
+
                 else -> false
             }
         }
-        hide.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                when (event?.action) {
-                    MotionEvent.ACTION_DOWN -> etPassword!!.transformationMethod = HideReturnsTransformationMethod.getInstance()
-                    MotionEvent.ACTION_UP ->  etPassword!!.transformationMethod =  PasswordTransformationMethod.getInstance()
-                }
-                return v?.onTouchEvent(event) ?: true
-            }
-        })
 
-        themeButt!!.setOnClickListener(View.OnClickListener {
-            if(resources.getString(R.string.mode) == "Day"){
+        visiblePassBtn.setOnTouchListener { v, event ->
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> etPassword!!.transformationMethod =
+                    HideReturnsTransformationMethod.getInstance()
+
+                MotionEvent.ACTION_UP -> etPassword!!.transformationMethod =
+                    PasswordTransformationMethod.getInstance()
+            }
+            v?.onTouchEvent(event) ?: true
+        }
+
+        themeButt.setOnClickListener {
+            if (resources.getString(R.string.mode) == "Day") {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
             } else {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
-        })
-
-
-    }
-
-
-    private fun fromScreenOrientation(btn1 : Button, btn2 : Button) : Button {
-        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            btn2.visibility = View.GONE
-            btn1.visibility = View.VISIBLE
-            return btn1;
-        } else {
-            btn1.visibility = View.GONE
-            btn2.visibility = View.VISIBLE
-            return btn2;
         }
     }
 
-    private fun CheckFields(): Boolean {
-        if (etEmail!!.length() == 0) {
-            etEmail!!.error = "This field is required"
+    private fun checkFields(): Boolean {
+        if (etEmail.length() == 0) {
+            etEmail.error = getString(R.string.field_is_required)
             return false
         }
-        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail!!.text).matches()) {
-            etEmail!!.error = "Not Email"
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(etEmail.text).matches()) {
+            etEmail.error = getString(R.string.not_email)
             return false
         }
-        if (etPassword!!.length() == 0) {
-            etPassword!!.error = "This field is required"
+        if (etPassword.length() == 0) {
+            etPassword.error = getString(R.string.field_is_required)
             return false
         }
-        if (etPassword!!.length() < 6) {
-            etPassword!!.error = "Password must be minimum 6 characters"
+        if (etPassword.length() < 6) {
+            etPassword.error = getString(R.string.min_pass)
             return false
         }
         return true
