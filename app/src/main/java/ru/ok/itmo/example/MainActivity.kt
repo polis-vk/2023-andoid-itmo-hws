134175da9d57ba140b4303b9b0c0dc456195572e
Pletnev_Kirill <334139@niuitmo.ru>
2023-10-16 17:04:34 +0300

    Hw 5: task 1 done

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,6 +1,44 @@
 package ru.ok.itmo.example
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var buttonStart : Button
+    private lateinit var progressBar : ProgressBar
+    private var isButtonClicked = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        buttonStart = findViewById(R.id.startButton)
+        progressBar = findViewById(R.id.progress_bar)
+
+        buttonStart.setOnClickListener {
+            if (!isButtonClicked) {
+                getProcessThread().start()
+            }
+        }
+    }
+
+
+    private fun getProcessThread(): Thread {
+        return Thread {
+            isButtonClicked = true
+            var currentProgress = 0
+            if (progressBar.progress != 100) {
+                currentProgress = progressBar.progress
+            }
+            for (i in currentProgress + 1..100) {
+                runOnUiThread {
+                    progressBar.progress = i
+                }
+                Thread.sleep(100)
+            }
+            isButtonClicked = false
+        }
+    }
+
 }
