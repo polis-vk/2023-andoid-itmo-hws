d01133e890376f6a7ca85db0bb9236bec99ef32e
Andrey Verenya <verenya08@gmail.com>
2023-10-18 14:50:27 +0300

    draft pages

---
+++ app/src/main/java/ru/ok/itmo/example/App.kt
@@ -0,0 +1,17 @@
+package ru.ok.itmo.example
+
+import androidx.fragment.app.Fragment
+
+class App(private var numberOfSections: Int) {
+
+
+}
+
+class Section : Fragment(){
+
+}
+
+class Page
+{
+
+}
