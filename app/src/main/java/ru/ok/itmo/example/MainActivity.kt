9352c2f5e76d3239f8481da1e2d089571810b623
georgiy4 <115575494+georgiy4@users.noreply.github.com>
2023-10-23 19:32:27 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,6 +1,64 @@
 package ru.ok.itmo.example
 
+import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private var delay_time: Long = 100
+
+    private fun task1() {
+        findViewById<TextView>(R.id.button).setOnClickListener {
+            val t = Thread(Runnable{
+                while (findViewById<ProgressBar>(R.id.progress_bar).progress < 100) {
+                    runOnUiThread {
+                        findViewById<ProgressBar>(R.id.progress_bar).progress++
+                    }
+                    Thread.sleep(delay_time)
+                }
+            })
+            t.start()
+        }
+    }
+
+    private fun task2() {
+        findViewById<TextView>(R.id.button).setOnClickListener {
+            Completable.create {
+                while (findViewById<ProgressBar>(R.id.progress_bar).progress < 100){
+                    findViewById<ProgressBar>(R.id.progress_bar).progress++
+                    Thread.sleep(delay_time)
+                }
+            }
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        task1()
+
+        findViewById<TextView>(R.id.button_restart).setOnClickListener {
+            findViewById<ProgressBar>(R.id.progress_bar).progress = 0
+        }
+        findViewById<TextView>(R.id.r50).setOnClickListener {
+            delay_time = 50
+        }
+        findViewById<TextView>(R.id.r100).setOnClickListener {
+            delay_time = 100
+        }
+        findViewById<TextView>(R.id.r300).setOnClickListener {
+            delay_time = 300
+        }
+        findViewById<TextView>(R.id.r500).setOnClickListener {
+            delay_time = 500
+        }
+    }
 }
