c93231bd7a89f1412074fc91d00753b1a1cb63a9
Efimenko_Viktor <lixkarder@gmail.com>
2023-10-27 11:57:14 +0300

    bonus_task_5

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -1,10 +1,11 @@
 package ru.ok.itmo.example
 
-import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
@@ -14,55 +15,66 @@ import java.util.concurrent.TimeUnit
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private lateinit var startButton: Button
     private lateinit var progressBar: ProgressBar
+    private lateinit var radioGroup: RadioGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         startButton = findViewById(R.id.startButton)
         progressBar = findViewById(R.id.progressBar)
+        radioGroup = findViewById(R.id.radioGroup)
 
-        fun workWithUI(){
+        fun workWithUI() {
             progressBar.progress++
-            if(progressBar.progress == 100){
+            if (progressBar.progress == 100) {
                 startButton.visibility = View.VISIBLE
                 startButton.text = getString(R.string.restartBarBtn)
             }
         }
 
+        fun getDelayValue(): Long {
+            val radioButton = findViewById<RadioButton>(radioGroup.checkedRadioButtonId)
+            return when (radioButton.text) {
+                "50ms" -> 50
+                "100ms" -> 100
+                "300ms" -> 300
+                "500ms" -> 500
+                else -> throw UnknownError("Неизвестное значение radioBtn")
+            }
+        }
 
-        fun task1(){
-            Thread{
-                while (progressBar.progress != 100){
+        fun task1() {
+            Thread {
+                while (progressBar.progress != 100) {
                     runOnUiThread { workWithUI() }
                     try {
-                        Thread.sleep(100)
-                    }catch (e: InterruptedException){
+                        Thread.sleep(getDelayValue())
+                    } catch (e: InterruptedException) {
                         e.printStackTrace()
                     }
                 }
             }.start()
         }
 
-        fun task2(){
-            Observable.interval(100, TimeUnit.MILLISECONDS)
+        fun task2() {
+            Observable.interval(getDelayValue(), TimeUnit.MILLISECONDS)
                 .takeWhile { progressBar.progress != 100 }
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{
+                .subscribe {
                     workWithUI()
                 }
         }
 
         startButton.setOnClickListener {
-            if(progressBar.progress != 100){
+            if (progressBar.progress != 100) {
                 task1() //or task2()
                 startButton.visibility = View.INVISIBLE
-            }else{
+            } else {
                 startButton.text = getString(R.string.startBarBtn)
                 progressBar.progress = 0
             }
 
         }
     }
-
 }
