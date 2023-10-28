f33939d2e3471bca98a12522479a2ea2dc0c9de5
Andrey Verenya <verenya08@gmail.com>
2023-10-28 04:27:42 +0300

    Small improvements

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -7,8 +7,11 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import ru.ok.itmo.example.alarm.AlarmWorker
@@ -19,27 +22,32 @@ import ru.ok.itmo.example.counters.FlowWorker
 import ru.ok.itmo.example.counters.ViewModelWorker
 import ru.ok.itmo.example.counters.ViewModelWorkerFactory
 import ru.ok.itmo.example.counters.Worker
+import java.util.UUID
 
-class MainActivity: AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private lateinit var progressBar: ProgressBar
     private lateinit var textView: TextView
+    private lateinit var dateTextView: TextView
+    private lateinit var statusAlarmTextView: TextView
 
     private lateinit var buttonCoroutinesWorker: Button
     private lateinit var buttonCoroutinesViewModelWorker: Button
     private lateinit var buttonFlowWorker: Button
 
     private lateinit var buttonSetAlarm: Button
+    private lateinit var buttonResetAlarm: Button
 
     private lateinit var coroutinesWorker: CoroutinesWorker
     private lateinit var coroutinesViewModelWorker: CoroutinesViewModelWorker
     private lateinit var flowWorker: FlowWorker
 
     private val counter: Counter = Counter()
+    private lateinit var uuidAlarm: UUID
+    private val workManager = WorkManager.getInstance(this)
 
-    companion object
-    {
-        private const val SLEEP_TIME_MS: Long = 10
+    companion object {
+        private const val SLEEP_TIME_MS: Long = 100
     }
 
     private fun setValueToBar(value: Int) {
@@ -70,9 +78,14 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
 
         progressBar = findViewById(R.id.progress_bar)
         textView = findViewById(R.id.text_view)
+        dateTextView = findViewById(R.id.date_text_view)
+        statusAlarmTextView = findViewById(R.id.status_alarm_text_view)
 
         coroutinesWorker = CoroutinesWorker(counter, startUpdateUi, updateUi, endUpdateUi)
-        coroutinesViewModelWorker = ViewModelProvider(this, ViewModelWorkerFactory(counter))[CoroutinesViewModelWorker::class.java]
+        coroutinesViewModelWorker = ViewModelProvider(
+            this,
+            ViewModelWorkerFactory(counter)
+        )[CoroutinesViewModelWorker::class.java]
         flowWorker = FlowWorker(counter)
 
         progressBar.max = Counter.MAX
@@ -81,6 +94,7 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
         buttonCoroutinesViewModelWorker = findViewById(R.id.button_coroutines_view_model_worker)
         buttonFlowWorker = findViewById(R.id.button_flow_worker)
         buttonSetAlarm = findViewById(R.id.button_set_alarm)
+        buttonResetAlarm = findViewById(R.id.button_reset_alarm)
 
         btnUsualLogic(buttonCoroutinesWorker, coroutinesWorker)
         btnViewModelLogic(buttonCoroutinesViewModelWorker, coroutinesViewModelWorker)
@@ -89,6 +103,16 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
         buttonSetAlarm.setOnClickListener {
             showTimePickerDialog()
         }
+        buttonResetAlarm.setOnClickListener {
+            if (::uuidAlarm.isInitialized)
+                workManager.cancelWorkById(uuidAlarm)
+            buttonSetAlarm.isEnabled = true
+        }
+    }
+
+    private fun updateAlarmTimeTextView(hour: Int, minute: Int) {
+        val formattedTime = String.format("%02d:%02d", hour, minute)
+        dateTextView.text = "Selected Time: $formattedTime"
     }
 
     private fun showTimePickerDialog() {
@@ -107,7 +131,10 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
         )
         timePickerDialog.show()
     }
+
     private fun scheduleAlarm(hour: Int, minute: Int) {
+        updateAlarmTimeTextView(hour, minute)
+
         val currentTime = Calendar.getInstance()
         val selectedTime = Calendar.getInstance()
         selectedTime.set(Calendar.HOUR_OF_DAY, hour)
@@ -118,7 +145,18 @@ class MainActivity: AppCompatActivity(R.layout.activity_main) {
             selectedTime.add(Calendar.DATE, 1)
         }
 
-        AlarmWorker.scheduleAlarmWork(this, selectedTime.timeInMillis)
+        uuidAlarm = AlarmWorker.scheduleAlarmWork(this, selectedTime.timeInMillis)
+
+        buttonSetAlarm.isEnabled = false
+        workManager.getWorkInfoByIdLiveData(uuidAlarm).observe(this) { workInfo ->
+            if (workInfo != null) {
+                statusAlarmTextView.text = workInfo.state.name
+
+                if (workInfo.state == WorkInfo.State.SUCCEEDED || workInfo.state == WorkInfo.State.FAILED) {
+                    buttonSetAlarm.isEnabled = true
+                }
+            }
+        }
     }
 
     private fun btnUsualLogic(button: Button, worker: Worker<Unit>) {
--- app/src/main/java/ru/ok/itmo/example/alarm/AlarmWorker.kt
+++ app/src/main/java/ru/ok/itmo/example/alarm/AlarmWorker.kt
@@ -5,6 +5,8 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Constraints
@@ -14,9 +16,11 @@ import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import ru.ok.itmo.example.R
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 class AlarmWorker(context: Context, workerParams: WorkerParameters) : Worker(context, workerParams) {
+    @RequiresApi(Build.VERSION_CODES.O)
     @SuppressLint("MissingPermission")
     override fun doWork(): Result {
         return try {
@@ -25,35 +29,34 @@ class AlarmWorker(context: Context, workerParams: WorkerParameters) : Worker(con
             val notification = NotificationCompat.Builder(applicationContext, "channel_id")
                 .setContentTitle("Alarm!")
                 .setContentText("alarm alarm alarm...")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setVibrate(LongArray(0))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
 
             notificationManager.notify(1, notification.build())
             return Result.success()
         } catch (e: Exception) {
+            Log.d("debug", e.message.toString())
             Result.failure()
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "channel_alarm"
-            val descriptionText = "notification from WorkManager"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("channel_id", name, importance).apply {
-                description = descriptionText
-            }
-
-            val notificationManager =
-                applicationContext.getSystemService(NotificationManager::class.java)
-            notificationManager.createNotificationChannel(channel)
+        val name = "channel_alarm"
+        val descriptionText = "Alarm notification"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel("channel_id", name, importance).apply {
+            description = descriptionText
         }
+
+        val notificationManager =
+            applicationContext.getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
     }
 
     companion object {
 
-        fun scheduleAlarmWork(context: Context, timeInMillis: Long) {
+        fun scheduleAlarmWork(context: Context, timeInMillis: Long): UUID {
             val constraints = Constraints.Builder()
                 .build()
 
@@ -71,6 +74,7 @@ class AlarmWorker(context: Context, workerParams: WorkerParameters) : Worker(con
                 .build()
 
             WorkManager.getInstance(context).enqueue(alarmRequest)
+            return alarmRequest.id
         }
     }
 }
