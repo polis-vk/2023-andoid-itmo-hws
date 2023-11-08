38b4cb550d529a7fb9a62d105232cf39128b8112
Drakon-top <kyzy77777@yandex.ru>
2023-11-08 13:19:49 +0300

    fix

--- app/src/main/java/ru/ok/itmo/example/MainActivity.kt
+++ app/src/main/java/ru/ok/itmo/example/MainActivity.kt
@@ -12,50 +12,59 @@ import io.reactivex.rxjava3.core.Observable
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-
+    private lateinit var buttonStart: Button
+    private lateinit var textCount: TextView
+    private lateinit var progressBar: ProgressBar
+    private lateinit var radioGroup: RadioGroup
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
-        val buttonStart = findViewById<Button>(R.id.button_start)
-        val textCount = findViewById<TextView>(R.id.text_count)
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        val radioGroup = findViewById<RadioGroup>(R.id.radio_button_count)
+        buttonStart = findViewById(R.id.button_start)
+        textCount = findViewById(R.id.text_count)
+        progressBar = findViewById(R.id.progressBar)
+        radioGroup = findViewById(R.id.radio_button_count)
 
         buttonStart.setOnClickListener {
-            val selectedOption: Int = radioGroup!!.checkedRadioButtonId
-            val radioButton = findViewById<RadioButton>(selectedOption)
-            val countLoad = (radioButton.text as String).toInt()
-
             buttonStart.isEnabled = false
-            // task_1 Thread and Runnable
-//            Thread {
-//                for (n in 1..countLoad) {
-//                    runOnUiThread {
-//                        progressBar.progress = n * 100 / countLoad
-//                        textCount.text = "$n"
-//                    }
-//                    Thread.sleep(100)
-//                }
-//                runOnUiThread {
-//                    buttonStart.isEnabled = true
-//                }
-//            }.start()
-
-            // task_2 Observable ans Observer
-            Observable.range(1, countLoad)
-                .concatMap { number: Int -> Observable.just(number).delay(100, TimeUnit.MILLISECONDS) }
-                .subscribe(
-                    { value ->
-                        runOnUiThread {
-                            progressBar.progress = value * 100 / countLoad
-                            textCount.text = "$value"
-                        }
-                    },
-                    { error -> System.err.println("Error: " + error.stackTrace) }
-                ) { runOnUiThread { buttonStart.isEnabled = true } }
+            withObserver()
         }
+    }
 
+    fun getCount(): Int {
+        val selectedOption: Int = radioGroup.checkedRadioButtonId
+        val radioButton = findViewById<RadioButton>(selectedOption)
+        return (radioButton.text as String).toInt()
+    }
+
+    fun withThread() {
+        val countLoad = getCount()
+        Thread {
+                for (n in 1..countLoad) {
+                    runOnUiThread {
+                        progressBar.progress = n * 100 / countLoad
+                        textCount.text = "$n"
+                    }
+                    Thread.sleep(100)
+                }
+                runOnUiThread {
+                    buttonStart.isEnabled = true
+                }
+            }.start()
+    }
+
+    @SuppressLint("CheckResult")
+    fun withObserver() {
+        val countLoad = getCount()
+        Observable.range(1, countLoad)
+            .concatMap { number: Int -> Observable.just(number).delay(100, TimeUnit.MILLISECONDS) }
+            .subscribe(
+                { value ->
+                    runOnUiThread {
+                        progressBar.progress = value * 100 / countLoad
+                        textCount.text = "$value"
+                    }
+                },
+                { error -> System.err.println("Error: " + error.stackTrace) }
+            ) { runOnUiThread { buttonStart.isEnabled = true } }
     }
 }
