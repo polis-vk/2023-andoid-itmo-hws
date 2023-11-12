7d6b183daaf344cdb6626fd94a8e7cc989c4dcc4
Grishechkin Pavel <grishechkin.v.2022@gmail.com>
2023-11-12 20:37:27 +0300

    fixed

--- app/src/main/kotlin/MainActivity.kt
+++ app/src/main/kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Patterns
 import android.view.KeyEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -11,28 +12,21 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    companion object {
-        val TAG = MainActivity::class.java.name
-    }
+    private lateinit var signInButton: Button;
+    private lateinit var switchThemeButton: Button;
+    private lateinit var login: TextInputLayout;
+    private lateinit var password: TextInputLayout;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val signInButton = findViewById<Button>(R.id.sign_in_button)
-        val switchThemeButton = findViewById<Button>(R.id.switch_theme_button)
-        val login = findViewById<TextInputLayout>(R.id.login_input)
-        val password = findViewById<TextInputLayout>(R.id.password_input)
+        signInButton = findViewById(R.id.sign_in_button)
+        switchThemeButton = findViewById(R.id.switch_theme_button)
+        login = findViewById(R.id.login_input)
+        password = findViewById(R.id.password_input)
 
         signInButton.setOnClickListener {
-            if (login.editText?.text?.isEmpty() == true) {
-                Toast.makeText(applicationContext, "Login field is empty", Toast.LENGTH_SHORT).show()
-            } else if (password.editText?.text?.isEmpty() == true) {
-                Toast.makeText(applicationContext, "Password field is empty", Toast.LENGTH_SHORT).show()
-            } else if (!Patterns.EMAIL_ADDRESS.matcher(login.editText?.text ?: "").matches()) {
-                Toast.makeText(applicationContext, "Incorrect email", Toast.LENGTH_SHORT).show()
-            } else if (password.editText!!.text.length < 6) {
-                Toast.makeText(applicationContext, "Incorrect password", Toast.LENGTH_SHORT).show()
-            }
+            signInButtonClick()
         }
 
         switchThemeButton.setOnClickListener {
@@ -43,12 +37,24 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             }
         }
 
-        password.editText?.setOnKeyListener(View.OnKeyListener { _, _, event ->
-            if (event?.action == KeyEvent.KEYCODE_ENTER) {
-                signInButton.performClick()
-                return@OnKeyListener true
+        password.editText?.setOnKeyListener { _, keyCode, _ ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                signInButtonClick()
+                return@setOnKeyListener true
             }
-            return@OnKeyListener false
-        })
+            return@setOnKeyListener false
+        }
+    }
+
+    private fun signInButtonClick() {
+        if (login.editText?.text?.isEmpty() == true) {
+            Toast.makeText(applicationContext, R.string.login_empty_toast, Toast.LENGTH_SHORT).show()
+        } else if (password.editText?.text?.isEmpty() == true) {
+            Toast.makeText(applicationContext, R.string.password_empty_toast, Toast.LENGTH_SHORT).show()
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(login.editText?.text ?: "").matches()) {
+            Toast.makeText(applicationContext, R.string.incorrect_email_toast, Toast.LENGTH_SHORT).show()
+        } else if (password.editText!!.text.length < 6) {
+            Toast.makeText(applicationContext, R.string.incorrect_password_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
