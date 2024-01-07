2f310b9ead0352d27462e2644b226649b042197d
Efimenko_Viktor <lixkarder@gmail.com>
2024-01-07 22:42:36 +0300

    edge-to-edge

--- app/src/main/java/com/example/settingsscreenitmoproject/MainActivity.kt
+++ app/src/main/java/com/example/settingsscreenitmoproject/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.settingsscreenitmoproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +16,7 @@ import com.example.settingsscreenitmoproject.ui.theme.SettingsScreenItmoProjectT
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             SettingsScreenItmoProjectTheme {
