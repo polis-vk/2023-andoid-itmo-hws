0a7c0aff9482f5b374b283722febbda55d11244a
ElTiburone <61785414+ElTiburone@users.noreply.github.com>
2024-01-19 14:02:19 +0300

    Delete SettingItem.kt

--- app/src/main/java/ru/ok/itmo/example/SettingItem.kt
+++ app/src/main/java/ru/ok/itmo/example/SettingItem.kt
@@ -1,4 +1,1 @@
-package ru.ok.itmo.example
-data class SettingCategory(val title: String, val items: List<SettingItem>)
 
-data class SettingItem(val title: String, val subtitle: String?, val parameter: String?)
